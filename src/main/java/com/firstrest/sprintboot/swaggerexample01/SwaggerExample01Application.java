a558dd4138d187015f5389a05f516e71675424d0
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2019-01-24 17:10:58 -0500

    Origin from https://start.spring.io/ (#1)
    Spring Boot 2.1.2
    Group: com.firstrest.sprintboot
    Artifact: com.firstrest.sprintboot
    Search Dependencies: Web

---
+++ src/main/java/com/firstrest/sprintboot/swaggerexample01/SwaggerExample01Application.java
@@ -0,0 +1,14 @@
+package com.firstrest.sprintboot.swaggerexample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExample01Application.class, args);
+	}
+
+}
+
