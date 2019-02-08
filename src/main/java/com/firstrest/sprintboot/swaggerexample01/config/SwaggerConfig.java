58548e873b57fb80c0ba96a9239ba0dc632973b8
Juan David Gonzalez Piza.PSL <jgonzalez@psl.com.co>
2019-02-08 18:04:22 -0500

    Just change some data in the Swagger Description Created by JDGonzal See more at http://google.com Contact the developer Apache License Version 2.0

--- src/main/java/com/firstrest/sprintboot/swaggerexample01/config/SwaggerConfig.java
+++ src/main/java/com/firstrest/sprintboot/swaggerexample01/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.firstrest.sprintboot.swaggerexample01.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,20 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.firstrest.sprintboot"))
                 .paths(regex("/rest.*"))
-                .build();
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo(){
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot Swagger Example API",
+                "Description Spring Boot Swagger Example API, to Learn a new",
+                "1.0",
+                "Terms of Service",
+                new Contact("JDGonzal", "http://google.com","jdgonzal2@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0"
+        );
+        return apiInfo;
     }
 }
